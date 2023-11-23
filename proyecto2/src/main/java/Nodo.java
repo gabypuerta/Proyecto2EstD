082374851c23713a586fc4dc6dd9122b6a0eeb2b
gabypuerta <Gabriela-puerta@correo.unimet.edu.ve>
2023-11-23 16:31:43 -0400

    Upload 1

---
+++ proyecto2/src/main/java/Nodo.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gaby3
+ */
+public class Nodo {
+    private int element;
+    private Nodo leftSon, rightSon;
+
+    public Nodo(int element) {
+        this.element = element;
+        this.leftSon = null;
+        this.rightSon = null;
+    }
+
+    public int getElement() {
+        return element;
+    }
+
+    public void setElement(int element) {
+        this.element = element;
+    }
+
+    public Nodo getLeftSon() {
+        return leftSon;
+    }
+
+    public void setLeftSon(Nodo leftSon) {
+        this.leftSon = leftSon;
+    }
+
+    public Nodo getRightSon() {
+        return rightSon;
+    }
+
+    public void setRightSon(Nodo rightSon) {
+        this.rightSon = rightSon;
+    }
+    
+    public boolean isLeaf() {
+        return getLeftSon() == null && getRightSon() == null;
+    }
+    
+}
+    
+
