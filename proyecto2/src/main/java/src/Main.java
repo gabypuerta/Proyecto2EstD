7978f2cc7faf6bf37eb1d8bd76bd00b1336aff80
VictoriaGarciav <113353097+VictoriaGarciav@users.noreply.github.com>
2023-11-23 17:13:13 -0400

    main y BST class :star2:

--- proyecto2/src/main/java/src/Main.java
+++ proyecto2/src/main/java/src/Main.java
@@ -15,7 +15,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        StartWindow fw = new StartWindow();
+        fw.setVisible(true);
+        fw.setLocationRelativeTo(null);
     }
     
 }
